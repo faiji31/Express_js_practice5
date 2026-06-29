f6dd1f1088e710e6be91dd3c2a82ca3a5b3553af
Faiji Akbar Liam <faijiakbarliam2016@gmail.com>
2026-06-29 22:43:55 +0600

    2nd commit

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,13 +1,16 @@
 import { Router, type Request, type Response } from "express";
 import { pool } from "../../db";
 import { userController } from "./user.controller";
+import auth from "../../middleware/auth";
 
 
 const router = Router()
 
 
+
+
 router.post("/",userController.createUser)
-router.get("/",userController.getUser)
+router.get("/",auth(),userController.getUser)
 router.get('/:id',userController.getSingleUser)
 router.put('/:id',userController.updateUser)
 
